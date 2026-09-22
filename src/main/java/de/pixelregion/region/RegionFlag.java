f1decba8569a7dcc38c8bc38e6c3bc549170f51f
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-22 04:48:09 +0200

    Build standalone polygon region core: src/main/java/de/pixelregion/region/RegionFlag.java

---
+++ src/main/java/de/pixelregion/region/RegionFlag.java
@@ -0,0 +1,15 @@
+package de.pixelregion.region;
+
+public enum RegionFlag {
+    BUILD,
+    USE,
+    PVP,
+    MOB_DAMAGE,
+    ENTITY_DAMAGE,
+    EXPLOSION,
+    FIRE_SPREAD,
+    FLUID_FLOW,
+    MOB_SPAWN,
+    ENTRY,
+    EXIT
+}
