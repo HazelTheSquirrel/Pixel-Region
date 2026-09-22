2ed6b78d7ea26dcaa715a923e70f1221fa550c3d
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-22 05:31:57 +0200

    Complete plugin wiring and durable shutdown persistence

--- src/main/java/de/pixelregion/PixelRegionPlugin.java
+++ src/main/java/de/pixelregion/PixelRegionPlugin.java
@@ -3,51 +3,80 @@ package de.pixelregion;
 import de.pixelregion.command.PixelRegionCommand;
 import de.pixelregion.listener.RegionBlockListener;
 import de.pixelregion.listener.RegionCombatListener;
+import de.pixelregion.listener.RegionContainerListener;
 import de.pixelregion.listener.RegionEnvironmentListener;
 import de.pixelregion.listener.RegionMovementListener;
 import de.pixelregion.region.FlagState;
+import de.pixelregion.region.JsonRegionStorage;
 import de.pixelregion.region.RegionFlag;
 import de.pixelregion.region.RegionManager;
 import de.pixelregion.region.RegionPolicyService;
-import de.pixelregion.region.RegionRepository;
 import de.pixelregion.region.RegionSessionManager;
 import de.pixelregion.region.RegionTransitionService;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.nio.file.Path;
 import java.util.EnumMap;
 
 public final class PixelRegionPlugin extends JavaPlugin {
     private RegionManager regionManager;
-    private RegionPolicyService regionPolicy;
     private RegionTransitionService regionTransitions;
     private RegionSessionManager sessionManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        final RegionRepository repository = new RegionRepository(this);
-        regionManager = new RegionManager(repository, defaultFlags());
-        regionManager.load();
-        regionPolicy = new RegionPolicyService(regionManager);
-        regionTransitions = new RegionTransitionService(regionManager, regionPolicy);
+
+        final Path storagePath = getDataFolder().toPath().resolve(
+                getConfig().getString("storage.file", "regions.json")
+        );
+        final JsonRegionStorage storage = new JsonRegionStorage(
+                storagePath,
+                message -> getLogger().warning(message),
+                message -> getLogger().severe(message)
+        );
+
+        regionManager = new RegionManager(storage, defaultFlags());
+        try {
+            regionManager.load();
+        } catch (IllegalStateException exception) {
+            getLogger().severe("Pixel-Region cannot start because region storage is unavailable.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        final RegionPolicyService regionPolicy = new RegionPolicyService(regionManager);
+        final RegionMessages messages = new RegionMessages(this);
+        regionTransitions = new RegionTransitionService(regionManager, regionPolicy, messages);
         sessionManager = new RegionSessionManager();
 
-        getServer().getPluginManager().registerEvents(new RegionBlockListener(regionPolicy), this);
+        getServer().getPluginManager().registerEvents(new RegionBlockListener(regionPolicy, messages), this);
         getServer().getPluginManager().registerEvents(new RegionCombatListener(regionPolicy), this);
+        getServer().getPluginManager().registerEvents(new RegionContainerListener(regionPolicy, messages), this);
         getServer().getPluginManager().registerEvents(new RegionEnvironmentListener(regionPolicy), this);
         getServer().getPluginManager().registerEvents(new RegionMovementListener(regionTransitions), this);
         getServer().getPluginManager().registerEvents(sessionManager, this);
 
-        registerCommand("pixelregion", new PixelRegionCommand(this, regionManager, sessionManager));
+        registerCommand(
+                "pixelregion",
+                new PixelRegionCommand(this, regionManager, sessionManager, regionPolicy, messages)
+        );
         getLogger().info("Pixel-Region enabled with " + regionManager.size() + " regions.");
     }
 
     @Override
     public void onDisable() {
-        if (regionManager != null) regionManager.save();
-        if (regionTransitions != null) regionTransitions.clearAll();
+        if (regionManager != null && regionManager.isDirty()) {
+            regionManager.save();
+        }
+        if (regionTransitions != null) {
+            regionTransitions.clearAll();
+        }
     }
 
-    public RegionManager regionManager() { return regionManager; }
+    public RegionManager regionManager() {
+        return regionManager;
+    }
 
     private EnumMap<RegionFlag, FlagState> defaultFlags() {
         final EnumMap<RegionFlag, FlagState> flags = new EnumMap<>(RegionFlag.class);
@@ -59,6 +88,7 @@ public final class PixelRegionPlugin extends JavaPlugin {
 
     private FlagState configState(String path) {
         return "deny".equalsIgnoreCase(getConfig().getString("protection." + path, "allow"))
-                ? FlagState.DENY : FlagState.ALLOW;
+                ? FlagState.DENY
+                : FlagState.ALLOW;
     }
 }
