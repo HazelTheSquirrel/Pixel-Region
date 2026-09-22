4c01a6d4c65c541561e2614cf039ca490d2cbaa8
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-22 05:32:44 +0200

    Fix storage test imports

--- src/test/java/de/pixelregion/region/JsonRegionStorageTest.java
+++ src/test/java/de/pixelregion/region/JsonRegionStorageTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.io.TempDir;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
