ea423c9169c8cc916bfd229789b5d7f4818e944b
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-22 05:31:07 +0200

    Complete configured messages and transition policy

--- src/main/java/de/pixelregion/RegionMessages.java
+++ src/main/java/de/pixelregion/RegionMessages.java
@@ -1,14 +1,43 @@
 package de.pixelregion;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RegionMessages {
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
+    private final String denied;
+    private final String enter;
+    private final String exit;
 
-    private RegionMessages() {
+    public RegionMessages(JavaPlugin plugin) {
+        this.denied = plugin.getConfig().getString("messages.denied", "<red>You are not allowed to do that here.");
+        this.enter = plugin.getConfig().getString("messages.enter", "<gray>You entered <gold>{region}</gold>.");
+        this.exit = plugin.getConfig().getString("messages.exit", "<gray>You left <gold>{region}</gold>.");
     }
 
-    public static void send(final CommandSender sender, final String message) {
+    public void send(CommandSender sender, String message) {
         sender.sendMessage(Component.text(message));
+    }
+
+    public void denied(Player player) {
+        player.sendMessage(miniMessage.deserialize(denied));
+    }
+
+    public void entered(Player player, String region) {
+        player.sendMessage(miniMessage.deserialize(
+                enter,
+                Placeholder.unparsed("region", region)
+        ));
+    }
+
+    public void exited(Player player, String region) {
+        player.sendMessage(miniMessage.deserialize(
+                exit,
+                Placeholder.unparsed("region", region)
+        ));
     }
 }
